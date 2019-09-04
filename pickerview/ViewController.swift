fbe355fbbaabc1962479a17bea8a4b5ce1331055
COE-026 <coe-026@coe-026s-imac.local>
2019-09-04 13:50:29 +0530

    Initial Commit

---
+++ pickerview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pickerview
+//
+//  Created by COE-026 on 04/09/19.
+//  Copyright © 2019 COE-026. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pickerviewTests/pickerviewTests.swift
@@ -0,0 +1,36 @@
+//
+//  pickerviewTests.swift
+//  pickerviewTests
+//
+//  Created by COE-026 on 04/09/19.
+//  Copyright © 2019 COE-026. All rights reserved.
+//
+
+import XCTest
+@testable import pickerview
+
+class pickerviewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
